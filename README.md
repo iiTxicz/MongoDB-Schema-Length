e6b600458660f58e82e0c576ff3ca40b31ab2d9b
J. Frost <robloxplayerexe3@gmail.com>
2023-10-28 22:21:02 -0600

    Added all needed items (IMPORT)
    Added all needed items (IMPORT)
    + mongodb.js
    + package-lock.json
    + package.json
    / README.md

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MongoDB Schema Length
  MongoDB Schema Length
+
+ # REQUIREMENTS
+ [NODE](https://nodejs.org/)
+
+# SETUP
+ > ```NPM INSTALL``` to install all needed packages
+ > MONGODB URI: Replace Line 4 of mongodb.js (```put-mongoDB-URI-HERE```) with your MongoDB Login URI
+ > node mongodb.js
+ > Make sure to have your designated console open to see results of length
+
+ # ISSUES
+ Contact ```jackasu``` on Discord
---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const { Schema, model, connect } = require("mongoose");
+
+connect('put-mongoDB-URI-HERE', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(async () => {
+    // Change your schema if this is the schema you don't want to console log
+    const loginSchema = new Schema({
+        username: { type: String },
+        password: { type: String }
+    });
+    
+    const login = model("Login", loginSchema);
+    
+    const fL1 = await login.count();
+    
+    console.log(`Login length: ${fL1}`);
+
+    mongoose.connection.close();
+    process.exit();
+}).catch((error) => {
+    console.log(error);
+});
